c727b603b80dff2bd70ec82c1d8add8d4ffd6c51
Erik Larsson <karhax@gmail.com>
2019-01-25 21:33:37 +0100

    Added selling of objects

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour {
+	
+	public float collected = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Sellable");
+		{
+			collected += other.gameObject.GetComponent<ValueScript> ().value;
+			other.gameObject.SetActive(false);
+		}
+	}
+
+}
