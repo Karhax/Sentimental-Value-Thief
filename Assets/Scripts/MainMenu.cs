9af0686ef280a346c4738f6175e6bbbbad94cf11
Guest Person <gst12100@hs.local>
2019-01-26 12:52:54 +0100

    added new scenes and prefabs

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private string sceneName = "jocke_test_scene";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
