6e8fe50edfbff5ea518b78a169ade86483f7cd53
Erik <karhax@gmail.com>
2019-01-26 17:09:09 +0100

    Added light and falling script

---
+++ Assets/Scripts/AttachedAndFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachedAndFall : MonoBehaviour {
+
+	private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+		transform.GetComponent<Rigidbody> ().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pos != transform.position) {
+			transform.GetComponent<Rigidbody> ().useGravity = true;
+		}
+	}
+}
